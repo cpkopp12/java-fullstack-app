7c16176753bea731e05bed14e6d027ffce83f3d4
cpkopp <cameronpkopp@gmail.com>
2023-07-14 13:16:46 -1000

    validation on update form

--- todo-app/src/components/todo/TodoComponent.jsx
+++ todo-app/src/components/todo/TodoComponent.jsx
@@ -2,7 +2,6 @@ import { useParams } from "react-router-dom"
 import { retrieveTodoApi } from "./api/TodoApiService";
 import { useAuth } from './security/AuthContext';
 import { useEffect, useState } from "react";
-import { Formik, Form, Field } from 'formik';
 
 export default function TodoComponent() {
 
@@ -10,6 +9,8 @@ export default function TodoComponent() {
 
     const [description, setDescription] = useState('');
     const [targetDate, setTargetDate] = useState('');
+    const [descriptionError, setDescriptionError] = useState(false);
+    
 
     const authContext = useAuth();
 
@@ -23,34 +24,43 @@ export default function TodoComponent() {
     function retrieveTodos() {
         retrieveTodoApi(username, id)
             .then(response => {
-                setDescription(response.data.description)
+                setDescription(response.data.description);
+                setTargetDate(response.data.targetDate);
+                console.log(response);
             })
             .catch(error => console.log(error));
+    }
+
+    function handleDescriptionChange(event) {
+        setDescription(event.target.value);
+    }
+
+    function handleTargetDateChange(event) {
+        setTargetDate(event.target.value);
+    }
+
+    function onSubmit(event) {
+        event.preventDefault();
+        console.log(description + targetDate);
+        if (description.length < 4 ) {
+            setDescriptionError(true);
+        } else {
+            setDescriptionError(false);
+        }
     }
 
     return (
         <div className="container">
             <h1>Enter Todo Details</h1>
             <div>
-                <Formik>
-                {
-                    (props) => (
-                        <div>
-                            <Form>
-                                <fieldset className="form-group">
-                                    <label>Description</label>
-                                    <Field type="text" className="form-control" name="description"/>
-                                </fieldset>
-                                <fieldset className="form-group">
-                                    <label>Target Date</label>
-                                    <Field type="date" className="form-control" name="targetDate"/>
-                                </fieldset>
-                            </Form>
-                            
-                        </div>
-                    )
-                }
-                </Formik>
+                {descriptionError && <div className="alert alert-danger">Enter atleast 4 characters for a description</div>}
+                <form onSubmit={onSubmit}>
+                    <label>Description</label>
+                    <input type="text" name="description" value={description} onChange={handleDescriptionChange}/>
+                    <label>Target Date</label>
+                    <input type="date" name="targetDate" value={targetDate} onChange={handleTargetDateChange}/>
+                    <button type="submit" >Save</button>
+                </form>
             </div>
         </div>
     )
